b161f7d3cf0a59b268697c4b8e29c25f5445ef0e
NickoalsPogozelski <nickolas@oneweb.com.br>
2026-07-17 20:32:00 -0300

    Ajustando velocidades padrão

--- scripts/global_var.gd
+++ scripts/global_var.gd
@@ -1,17 +1,17 @@
 extends Node
 
 # Constantes
-const default_vel_mundo = 300.0
-const default_vel_inimigo = 350.0
+const default_vel_mundo = 600.0
+const default_vel_inimigo = 650.0
 
 # Variáveis de velocidade do mundo
 var vel_mundo = default_vel_mundo
-var velMax_mundo = 1000.0
+var velMax_mundo = 1200.0
 var accel_mundo = 6.0
 
 # Variáveis de velocidade dos inimigos
 var vel_inimigo = default_vel_inimigo
-var velMax_inimigo = 1050.0
+var velMax_inimigo = 1250.0
 var accel_inimigo = 6.0
 
 # Variáveis player
